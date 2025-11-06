79faf663e2eb1bc5d8e5a8cfea39f2a7a2643fb2
Krisztián Szabó <sz.krisztian96@gmail.com>
2025-11-06 16:21:20 +0100

    Implemented a very basic OAM DMA copy solution but still stuck on Tetris boot screen.

--- src/mmu.cpp
+++ src/mmu.cpp
@@ -17,10 +17,22 @@ namespace sickboy {
     }
 
     void MMU::write(std::uint16_t address, std::uint8_t value) {
+        static constexpr std::uint16_t BOOT_ROM_DISABLE_ADDRESS = 0xFF50;
+        static constexpr std::uint16_t OAM_DMA_COPY_ADDRESS = 0xFF46;
+
         ram[address] = value;
-        // Writes to 0xFF50 disable the boot ROM
-        if (address == 0xFF50) {
+        // Handle writes that disable the boot ROM
+        if (address == BOOT_ROM_DISABLE_ADDRESS) {
             set_boot_rom_enabled(false);
+        }
+        // Handle writes that trigger an OAM DMA copy
+        else if (address == OAM_DMA_COPY_ADDRESS) {
+            // TODO: This is wildly inaccurate - in reality this copy takes 160 memory cycles or 640 master clock cycles
+            // During the period of the copy RAM is unavailable and the program can only read from HRAM. This should
+            // be implemented by adding ticking to the MMU and locking read/write operations for 640 master clock cycles.
+            std::uint16_t source = value << 8;
+            static constexpr std::uint16_t OAM_MEMORY = 0xFE00;
+            copy_to(OAM_MEMORY, ram.data() + source, 160);
         }
     }
 
