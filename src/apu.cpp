9c270fb36c4dcc87aa5df446593fb966ba478c2d
Krisztián Szabó <sz.krisztian96@gmail.com>
2026-07-20 17:44:18 +0200

    Now getting roughly the expected frequency (there are still a few rounding errors) on channel 1.

--- src/apu.cpp
+++ src/apu.cpp
@@ -215,8 +215,8 @@ namespace sickboy {
             };
             std::uint8_t current_waveform_bit = get_waveform_bit(channels[0].duty_cycle_sample_idx);
             std::uint16_t channel_period = get_channel_period(0);
-            // TODO: This likely should not be calculated from the APU frequency, but from our sampling rate (48kHz) !!!
-            std::uint16_t periods_per_sample = static_cast<std::uint16_t>(APU_FREQUENCY_HZ / 4 / channel_period);
+            // TODO: Avoid rounding this value as it contributes to audio accuracy (e.g. it getting rounded to 22 instead of 21.8453 means that a 441Hz sound will be 444Hz)
+            std::uint16_t periods_per_sample = static_cast<std::uint16_t>(std::round(APU_FREQUENCY_HZ / 4 / (double) AUDIO_SAMPLE_RATE));
             for (std::size_t sample_idx = 0; sample_idx < AUDIO_SAMPLES_PER_BUFFER; ++sample_idx) {
                 // TODO: Implement envelope which would modify the volume field
                 // TODO: Rewrite this to cleaner code - volume ranges [0, 15], but 16bit PCM is [-2^16/2, 2^16/2]
